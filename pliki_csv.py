79ede5e1b9cbdf3f8342d31a2b2a20dbd14399f1
student-wroclaw <aga.d.kaminska@gmail.com>
2022-12-10 14:55:56 +0100

    plikicsv

--- pliki_csv.py
+++ pliki_csv.py
@@ -1,5 +1,6 @@
-path = 'C: \\Users\\vdi-student\\Pulpit\\ rozliczenie.csv'
+path = 'C:\Users\vdi-student\Desktop\\rozliczenie.csv'
 mode = 'r'
+#zapis danych w odpowiedniej postaci
 
 with open(path, mode) as plik:
     content = plik.readlines()
@@ -14,6 +15,18 @@ print(content[3][3])
 
 total = 0
 for i in range (1,len(content)):
+    total = total + int(content[i][1])
+average = total / (len(content)-1)
+print = (rounde(average, 2))
+
+#ilość kobiet na macierzyńskim
+
+for i in range (1,len(content)):
+    if content[i][3] =='k' and content[i][4] == 't':
+        total += 1
+print(total)
+
+
 
 
 
